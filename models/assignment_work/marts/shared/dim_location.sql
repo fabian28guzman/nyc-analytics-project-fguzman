5a79b820d54ba9ae50afec94fa08df2017badb29
Fabian Guzman <fabian.guzman@baruchmail.cuny.edu>
2026-03-28 21:49:57 +0000

    Updated dim_location.sql

--- models/assignment_work/marts/shared/dim_location.sql
+++ models/assignment_work/marts/shared/dim_location.sql
@@ -3,7 +3,7 @@
 WITH all_locations AS (
    -- Get locations from 311 requests
    SELECT DISTINCT
-      borough, incident_zip --TODO replace (HINT: look @ dimensional model & staging data!)
+      borough, incident_zip as zip_code --TODO replace (HINT: look @ dimensional model & staging data!)
    FROM {{ ref('stg_nyc_311_dot') }}
    WHERE borough IS NOT NULL
 
@@ -11,7 +11,7 @@ WITH all_locations AS (
 
    -- Get locations from restaurant applications
    SELECT DISTINCT
-       borough, zip -- TODO replace (HINT: look @ dimensional model & staging data!)
+       borough, zip as zip_code -- TODO replace (HINT: look @ dimensional model & staging data!)
    FROM {{ ref('stg_nyc_open_restaurant_apps') }}
    WHERE borough IS NOT NULL
 ),
